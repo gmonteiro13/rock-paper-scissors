b8497f50d5d94808d89dd2fecefc43579c556c65
gmonteiro13 <gscmonteiro@gmail.com>
2024-09-29 18:22:58 -0300

    Adds main.css file, configures LoginView post method

--- click_games/views.py
+++ click_games/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.views import View
 from click_games.forms import LoginForm
 
@@ -13,6 +15,17 @@ class LoginView(View):
     def post(self, request):
         form = LoginForm(request.POST)
         if form.is_valid():
-            return render(request, 'sucesso.html')
-        else:
-            return render(request, 'login.html', {'form': form})
+            usuario = form.cleaned_data['usuario']
+            senha = form.cleaned_data['senha']
+            user = authenticate(
+                request,
+                username=usuario,
+                password=senha
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Usuário ou senha inválidos')
+                render(request, 'login.html', {'form': form})
+        return render(request, 'login.html', {'form': form})
